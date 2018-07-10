059b7cf063faa866a73f4775ae767abd2f63aea1
georgi-bozhinov <georgi.bozhinov@sap.com>
2018-07-10 11:22:13 +0300

    feature(app.js): Add another route

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/home", (req, res) => {
   res.send("Stuff");
 });
 
+app.get("/login", (req, res) => {
+  res.send("Login");
+});
+
 app.listen(8000, () => {
   console.log("Listening on port 3000");
 });
